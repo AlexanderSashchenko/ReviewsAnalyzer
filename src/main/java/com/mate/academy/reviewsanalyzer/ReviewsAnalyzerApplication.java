10452e6a0f5f5f3a9f10ee6e520f72b8238e2ae0
Alexandr Sashchenko <santys87@gmail.com>
2020-03-01 19:01:41 +0200

    first commit

---
+++ src/main/java/com/mate/academy/reviewsanalyzer/ReviewsAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.mate.academy.reviewsanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewsAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewsAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mate/academy/reviewsanalyzer/ReviewsanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mate.academy.reviewsanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewsanalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
